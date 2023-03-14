51628e7c7fbae29b0997029c44e4931bbfc974a3
Daniel Casey <DCASEY@kingcounty.gov>
2023-03-14 08:56:51 -0700

    add reduce overlaps

--- R/utilities.R
+++ R/utilities.R
@@ -20,10 +20,11 @@ validate_col = function(d, id, type = 'source'){
 #' check the internal consitency of an sf file
 #' @param x a sf data.frame
 #' @param stop logical. Should this function throw an error or return the results?
+#' @param return_probs logical. Should the function return a vector of logicals indicating the rows with problems instead of the query results?
 #' @export
 #' @importFrom sf st_overlaps st_contains
-#' 
-check_internal_consistency = function(x, stop = TRUE){
+#'
+check_internal_consistency = function(x, stop = TRUE, return_probs = FALSE){
   stopifnot(inherits(x,'sf'))
   
   # check overlaps (e.g. one geometry straddles the border of another)
@@ -43,6 +44,7 @@ check_internal_consistency = function(x, stop = TRUE){
     }
     return(TRUE)
   }else{
+    if(return_probs) return(co_probs|cc_probs)
     return(list(co, cc))
   }
   
@@ -56,4 +58,57 @@ check_internal_consistency = function(x, stop = TRUE){
 bbox_to_sf = function(bbox,id = 1){
   sf::st_sf(data.frame(id = id, geom = sf::st_as_sfc(bbox)))
 } 
+
+#' Try to eliminate internal/self overlaps
+#' @param x sf object
+#' @param snap distince in st_crs(x,parameters = T)$ud_unit units to allow for snapping
+#' @return An sf object that hopefully passes check_internal_consistency checks.
+#' @details Users should visually review the resulting object to make sure the hueristics used here weren't too goofy
+#' @importFrom sf st_make_valid st_buffer st_is_longlat st_intersection
+#' @importFrom lwgeom st_snap_to_grid
+#' @export 
+reduce_overlaps = function(x, snap = .5){
+  
+  # start by making sure there are problems to fix
+  ic = check_internal_consistency(x, stop = F, return_probs = T)
+  if(!any(ic)) return(x)
+  
+  # try making it valid
+  x = sf::st_make_valid(sf::st_buffer(x,0))
+  ic = check_internal_consistency(x, stop = F, return_probs = T)
+  if(!any(ic)) return(x)
+  
+  # if its projected, try snapping to grid
+  prj = st_is_longlat(x)
+  prj = !(is.na(prj) | prj)
+  if(prj){
+    
+    # see if some snapping will work
+    x = lwgeom::st_snap_to_grid(x, snap)
+    x = sf::st_make_valid(x)
+    ic = check_internal_consistency(x, stop = F, return_probs = T)
+    if(!any(ic)) return(x)
+  }
+  
+  # internal intersections
+  x = st_intersection(x)
+  x = subset(x, n.overlaps<=1)
+  x$n.overlaps = NULL
+  x$origins = NULL
+  x = st_make_valid(x)
+  x = st_buffer(x,0)
+  ic = check_internal_consistency(x, stop = F, return_probs = T)
+  if(!any(ic)) return(x)
+  
+  # slight negative buffer
+  x = st_buffer(x, -snap *.01)
+  x = st_buffer(x,snap *.001)
+  x = st_make_valid(x)
+  
+  ic = check_internal_consistency(x, stop = F, return_probs = T)
+  if(!any(ic)) return(x)
+  
+  stop('Could not make `x` pass check_internal_consistency')
+
+}
 
--- misc/zip_to_hra.R
+++ misc/zip_to_hra.R
@@ -2,6 +2,7 @@ library('sf')
 library('spatagg')
 library('dplyr')
 library('kcparcelpop')
+library('data.table')
 out = 'C:/Users/dcasey/local_documents/scratch'
 hra = st_read("//dphcifs/APDE-CDIP/Shapefiles/HRA/HRA_2010.shp") %>% st_make_valid() %>% group_by(name) %>% summarize()
 zips = st_read("//phdata01/drof_data/DOH DATA/ADCi_WAStateZIPCode_Shapefile/J24007_King_County_Public_Health/adci_wa_zip_confidential/adci_wa_zip_confidential.shp")
@@ -24,14 +25,21 @@ pp = st_transform(kcparcelpop::parcel_pop, st_crs(hra))
 z2hra_fo = create_xwalk(zips[, 'POSTCODE'], hra[, c('new')], 'POSTCODE', 'new',method = 'fractional overlap', min_overlap = .1)
 z2hra_pp = create_xwalk(zips[, 'POSTCODE'], hra[, c('new')], 'POSTCODE', 'new',method = 'point pop', min_overlap = .1, point_pop = pp, pp_min_overlap = .2)
 
+setDT(z2hra_fo)
+setDT(z2hra_pp)
+
+stopifnot(all(hra$new %in% z2hra_fo[, target_id]))
+stopifnot(all(hra$new %in% z2hra_pp[, target_id]))
+
 saveRDS(z2hra_fo, file.path(out, 'zip2hra_fo_new.rds'))
 saveRDS(z2hra_pp, file.path(out, 'zip2hra_pp_new.rds'))
 
 
 
-
 library('mapview')
-m1 = hra %>% filter(new == 'Issaquah')
-mapview(zips) + mapview(m1)
+ud = hra %>% filter(new == 'U District')
+udh = st_intersection(pp, summarize(ud))
+udh = udh %>% mutate(case_when(is.na(pop) ~ 0, T ~ pop))
+mapview(ud) + mapview(udh, zcol = 'pop')
 setDT(z2hra_pp)
 blah2 = merge(z2hra_pp[target_id == 'Issaquah'], blah, all.x = T, by.x = 'source_id', by.y = 'geo_id')
