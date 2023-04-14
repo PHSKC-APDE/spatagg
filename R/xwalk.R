8ca0e5ff106e2f62c7757e0efbd7e2ef22ac4cf3
Daniel Casey <DCASEY@kingcounty.gov>
2023-04-14 15:16:07 -0700

    fix check

--- R/xwalk.R
+++ R/xwalk.R
@@ -158,8 +158,8 @@ xwalk_polap = function(source, target, source_id = 'id', target_id = 'id', point
   point_pop$ppid = seq_len(nrow(point_pop))
   # One point might belong to two target polygons if they overlap (e.g. at the vertex)
   point_pop = sf::st_join(point_pop, source[, source_id])
-  point_pop = sf::st_join(point_pop, target[, target_id])
   names(point_pop)[names(point_pop) == source_id] = 'source_id'
+  point_pop = sf::st_join(point_pop, target[, target_id])
   names(point_pop)[names(point_pop) == target_id] = 'target_id'
   sf::st_geometry(point_pop) = NULL
   point_pop = data.table::data.table(point_pop)
